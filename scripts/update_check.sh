b440d1b6378f39586316be8780f0da9c5f1ae5cd
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-08-06 23:38:54 +0100

    Simplify update checker

--- scripts/update_check.sh
+++ scripts/update_check.sh
@@ -3,23 +3,31 @@
 
 # Constants
 UPDATER_URL="https://updater.craftlandia.com.br/v3/updates.json"
-OLD_HASH="$(ls -Art intermediary | tail -n 1 | sed 's/\.tiny//')"
 
 # CODE
 UPDATER_FILE="$(curl --silent --insecure -s $UPDATER_URL)"
 NEW_HASH="$(jq '.files.Farewell | .[] | select(contains({file: "1.5.2-CraftLandia.jar"})) | .hash' <<<"${UPDATER_FILE}" | sed 's/"//g')"
 
-# if hash is different from last update, exit with error code 1
-if [ "${NEW_HASH}" != "${OLD_HASH}" ]; then
-    # if GITHUB_STEP_SUMMARY is set
-    if [ -n "$GITHUB_STEP_SUMMARY" ]; then
+# if GITHUB_STEP_SUMMARY is set
+if [ -n "$GITHUB_STEP_SUMMARY" ]; then
+
+    {
+        echo "# CraftLandia Update"
+        echo ""
+    } >"$GITHUB_STEP_SUMMARY"
+
+    # if intermediary of hash exists, assume the version is up to date
+    if [ -f "intermediary/${NEW_HASH}.tiny" ]; then
         {
-            echo "# CraftLandia Update"
-            echo ""
-            echo "| Old version Hash | New version Hash |"
-            echo "| :--------------: | :--------------: |"
-            echo "| $OLD_HASH | $NEW_HASH"
-        } > "$GITHUB_STEP_SUMMARY"
+            echo "Up to date!"
+        } >>"$GITHUB_STEP_SUMMARY"
+        exit 0
+    else
+        {
+            echo "| New version Hash |"
+            echo "| :--------------: |"
+            echo "| $NEW_HASH |"
+        } >>"$GITHUB_STEP_SUMMARY"
+        exit 1
     fi
-    exit 1
 fi
