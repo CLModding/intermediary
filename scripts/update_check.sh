8cbbdb6584283aa37a0ce06435d1e29ca8b57e59
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-08-20 23:22:13 +0100

    Fix spelling issue

--- scripts/update_check.sh
+++ scripts/update_check.sh
@@ -14,12 +14,12 @@ echo "Amogus"
     curl "https://discord.com/api/v10/webhooks/1006269909275267082/BCqilcwkVfl6FNCqZrskklf7Qe97LiEsV1AvZwsl10CkoMRseSbGq2FIi0red0xrfk_V/messages/1006962159537897602" \
         -X 'PATCH' \
         -H 'Content-Type: application/json' \
-        --data-raw '{"content":null,"embeds":[{"title":"CraftLandia Farewell - AntiHack Version","description":"AntiHack definitions is up-to-date.","color":2752256,"thumbnail":{"url":"https://i.imgur.com/Kgg9vCJ.png"}}],"username":"CLModding - Update Checker","avatar_url":"https://i.imgur.com/Kgg9vCJ.png","attachments":[]}' \
+        --data-raw '{"content":null,"embeds":[{"title":"CraftLandia Farewell - AntiHack Version","description":"AntiHack definitions are up-to-date.","color":2752256,"thumbnail":{"url":"https://i.imgur.com/Kgg9vCJ.png"}}],"username":"CLModding - Update Checker","avatar_url":"https://i.imgur.com/Kgg9vCJ.png","attachments":[]}' \
         --compressed
 else
     curl "$UPDATE_WEB_HOOK" \
         -X 'PATCH' \
         -H 'Content-Type: application/json' \
-        --data-raw "{\"content\":\"<@318033838330609665>\",\"embeds\":[{\"title\":\"CraftLandia Farewell - AntiHack Version\",\"description\":\"AntiHack definitions requires an update\u0021\",\"color\":16711680,\"fields\":[{\"name\":\"New Version Hash\",\"value\":\"$NEW_HASH\"}],\"thumbnail\":{\"url\":\"https://i.imgur.com/Kgg9vCJ.png\"}}],\"username\":\"CLModding - Update Checker\",\"avatar_url\":\"https://i.imgur.com/Kgg9vCJ.png\",\"attachments\":[]}" \
+        --data-raw "{\"content\":\"<@318033838330609665>\",\"embeds\":[{\"title\":\"CraftLandia Farewell - AntiHack Version\",\"description\":\"AntiHack definitions require an update\u0021\",\"color\":16711680,\"fields\":[{\"name\":\"New Version Hash\",\"value\":\"$NEW_HASH\"}],\"thumbnail\":{\"url\":\"https://i.imgur.com/Kgg9vCJ.png\"}}],\"username\":\"CLModding - Update Checker\",\"avatar_url\":\"https://i.imgur.com/Kgg9vCJ.png\",\"attachments\":[]}" \
         --compressed
 fi
