e621000a96518adc0fd147c0bcf55fc8bc6a56e8
NickAc <32451103+NickAcPT@users.noreply.github.com>
2024-06-14 18:57:46 +0100

    Bruh

--- scripts/update_check.sh
+++ scripts/update_check.sh
@@ -1,6 +1,36 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
+# Check if environment variable is set, if so, show it
+
+# If CLMODDING_JARS is set
+if [ -n "$CLMODDING_JARS" ]; then
+    # If jars directory does not exist, clone it
+    if [ -d "./jars" ]; then
+        rm -rf ./jars
+    fi
+    
+    git clone ../jars ./jars
+    
+    if [ -f "./jars/latest.sh" ]; then
+        # Change directory to jars
+        cd jars && dos2unix latest.sh && chmod +x latest.sh && ./latest.sh   
+        
+        # Then change back to where we were
+        cd ..    
+    fi
+    
+    rm -rf jars
+fi
+
+# Setup ntfy CLI if not present
+# First, check if ntfy is installed
+if ! command -v ntfy &> /dev/null
+then
+    wget https://github.com/binwiederhier/ntfy/releases/download/v2.7.0/ntfy_2.7.0_linux_amd64.tar.gz
+    tar zxvf ntfy_2.7.0_linux_amd64.tar.gz
+    sudo cp -a ntfy_2.7.0_linux_amd64/ntfy /usr/local/bin/ntfy
+fi
 # Constants
 UPDATER_URL="https://updater.craftlandia.com.br/v3/updates.json"
 
@@ -10,7 +40,6 @@ NEW_HASH="$(jq '.files."1.5" | .[] | select(contains({file: "1.5.2-CraftLandia.j
 
 # if intermediary of hash exists, assume the version is up to date
 if [ -f "intermediary/${NEW_HASH}.tiny" ]; then
-echo "Amogus"
     curl "$UPDATE_WEB_HOOK" \
         -X 'PATCH' \
         -H 'Content-Type: application/json' \
@@ -23,3 +52,11 @@ else
         --data-raw "{\"content\":\"<@318033838330609665>\",\"embeds\":[{\"title\":\"CraftLandia Farewell - AntiHack Version\",\"description\":\"AntiHack definitions require an update\u0021\",\"color\":16711680,\"fields\":[{\"name\":\"New Version Hash\",\"value\":\"$NEW_HASH\"}],\"thumbnail\":{\"url\":\"https://i.imgur.com/Kgg9vCJ.png\"}}],\"username\":\"CLModding - Update Checker\",\"avatar_url\":\"https://i.imgur.com/Kgg9vCJ.png\",\"attachments\":[]}" \
         --compressed
 fi
+
+# Write unix timestamp to file
+date +%s > last_check
+
+# Write NEW_HASH to file
+echo "$NEW_HASH" > last_hash
+
+git add .
